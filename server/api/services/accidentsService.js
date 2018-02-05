3c5200a08f6bd344723a91a47598e1c2a916d73c
Kevin Duglué <kevin.duglue@gmail.com>
2018-02-05 16:13:14 +0100

    Auto stash before merge of "master" and "origin/master"

--- server/api/services/accidentsService.js
+++ server/api/services/accidentsService.js
@@ -3,14 +3,10 @@ var ObjectId = require('mongodb').ObjectID;
 
 var assert = require('assert');
 var util = require('util');
-var Q = require('Q');
-//var url = 'mongodb://localhost:27017/test';
 
-// Connection URL
 const url = 'mongodb://localhost:27017';
 
-// Database Name
-const dbName = 'projetWeb';
+const dbName = 'accidents';
 
 exports.getAllAccidents = function() {
     return new Promise((resolve, reject) => {
@@ -19,8 +15,17 @@ exports.getAllAccidents = function() {
                 reject("Connexion impossible à MongoDB");
 
             var db = client.db(dbName);
-            db.collection('Caracteristiques').find({}).toArray(function(err, docs) {
-                console.log("Found the following records");
+            db.collection('Accidents').find({ 
+                geometry: {
+                    
+                    $geoWithin:
+                    { 
+                        $centerSphere: [ [ 7.261953, 43.710173 ], 5 / 3963.2 ] 
+                    } 
+                    
+                }
+            }).toArray(function(err, docs) {
+                console.log(docs.length);
                 resolve(docs);
             });
         });
