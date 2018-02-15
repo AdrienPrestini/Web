d71946a5243eff05923d8812486379ac2fa41e01
Adrien Prestini <adrienprestini@hotmail.fr>
2018-02-15 08:05:32 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ ClientSide/angularClient/app-project/src/app/loader.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class LoaderService {
+
+  constructor(private http:Http) { }
+
+  private url = "http://192.168.137.62:3000/accidents/";
+
+  async getAccidents(): Promise<any>{
+    //return await this.http.get(this.url).map((response : Response) => response.json());
+    const response = await this.http.get(this.url).toPromise();
+    return response.json();
+  }
+
+  getItinary(pointA, pointB){
+    var urlRequest = this.url + pointA.lat + "/"+ pointA.lng + "/" + pointB.lat + "/"+ pointB.lng;
+    console.log(urlRequest);
+    return this.http.get(urlRequest).map((response : Response) => response.json());
+    
+  }
+}
