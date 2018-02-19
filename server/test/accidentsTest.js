cff2f33c85ed4f97e6410af3b23c5e191ed65ec3
Kevin Duglué <kevin.duglue@gmail.com>
2018-02-19 16:10:32 +0100

    All integration tests

--- server/test/accidentsTest.js
+++ server/test/accidentsTest.js
@@ -9,9 +9,9 @@ chai.use(chaiHttp);
 
 
 describe('Books', () => {
-    
+
     describe('Accidents', () => {
-        it('it should GET all the accidents in ', (done) => {
+        it('it should GET all the accidents', (done) => {
             chai.request(server)
                 .get('/accidents/')
                 .end((err, res) => {
@@ -21,11 +21,25 @@ describe('Books', () => {
                     done();
                 });
         });
+
+        it('it should return all accidents in department Var', (done) => {
+            //GET
+        });
+
+        it('it should create an accident', (done) => {
+            //POST
+        });
+
+        it('it should update an accident', (done) => {
+            //PUT
+        });
+
+        it('it should add a comment to an accident', (done) => {
+            //POST
+        });
+
+        it('it should delete an accident', (done) => {
+            //DELETE
+        });
     });
 });
-
-// /GET accidents
-// /GET/departements (test plusieurs)
-// /POST
-// /PUT
-// /DELETE
