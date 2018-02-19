c2397d560c320178528f045985a115f3db144ced
Kevin Duglué <kevin.duglue@gmail.com>
2018-02-19 13:42:18 +0100

    Libelle delete

--- server/api/services/accidentsService.js
+++ server/api/services/accidentsService.js
@@ -323,7 +323,6 @@ function formatAccidentModel(infos){
     accident.properties.nbv = infos.nbv;
     accident.properties.code_postal = infos.code_postal;
     accident.properties.coord = [infos.lat, infos.long];
-    accident.properties.libellevoie = infos.libelle;
     return accident;
 }
 
